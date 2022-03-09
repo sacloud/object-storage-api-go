849fa138037217e2a46570721acc055cd75a9634
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2022-03-09 11:41:04 +0900

    Bump to v0.0.2

--- version.go
+++ version.go
@@ -16,7 +16,7 @@ package objectstorage
 
 var (
 	// Version app version
-	Version = "v0.0.1"
+	Version = "v0.0.2"
 	// Revision git commit short commit hash
 	Revision = "xxxxxx" // set on build time
 )
