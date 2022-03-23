e06c8780e1397971aca30047268c665aa129eb03
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2022-03-23 14:18:34 +0900

    v0.0.4

--- version.go
+++ version.go
@@ -16,7 +16,7 @@ package objectstorage
 
 var (
 	// Version app version
-	Version = "v0.0.3"
+	Version = "v0.0.4"
 	// Revision git commit short commit hash
 	Revision = "xxxxxx" // set on build time
 )
