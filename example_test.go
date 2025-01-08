a696b47bafd23cc39a59c0c242e785f1e5d042ec
Kazumichi Yamamoto <yamamoto.febc@gmail.com>
2025-01-08 10:30:20 +0900

    Copyright 2025 (#178)
    * Copyright 2025
    
    * ignore oas3-server-variables

--- example_test.go
+++ example_test.go
@@ -1,4 +1,4 @@
-// Copyright 2022-2023 The sacloud/object-storage-api-go authors
+// Copyright 2022-2025 The sacloud/object-storage-api-go authors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
